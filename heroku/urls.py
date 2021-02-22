836bdebc54f056b69ce4629b01680064477ec82d
test <MY_NAME@example.com>
2021-02-22 15:13:12 +0100

    Update in views and urls

--- heroku/urls.py
+++ heroku/urls.py
@@ -30,18 +30,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from . import views
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.conf import settings
 from django.conf.urls.static import static
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index,name='main'),
-    path('api', views.API_prototype.as_view(),name='api'),
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('Api-auth/', include('rest_framework.urls')),
-    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh')
+    path('', views.API_prototype.as_view(),name='main'),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- heroku/views.py
+++ heroku/views.py
@@ -3,8 +3,7 @@ from django.contrib.auth.models import User
 from rest_framework import generics
 from .serializers import user_serializer
 from rest_framework import permissions
-def index(request):
-    return render(request, 'detail.html');
 
 class API_prototype(generics.ListCreateAPIView):
-    queryset=[]
+    queryset=User.objects.all()
+    serializer_class = user_serializer
