a6ca9fa6f5977edc5ba20eb9627deb7f37fa1a56
test <MY_NAME@example.com>
2021-02-24 14:18:20 +0100

    Version Beta

--- heroku/views.py
+++ heroku/views.py
@@ -1,9 +1,1 @@
-from django.shortcuts import render
-from django.contrib.auth.models import User
-from rest_framework import generics
-from .serializers import user_serializer
-from rest_framework import permissions
 
-class API_prototype(generics.ListCreateAPIView):
-    queryset=User.objects.all()
-    serializer_class = user_serializer
